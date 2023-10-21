4851b575064f84ec7aa9369acabc422551690077
SciWake <SciWake@gmail.com>
2023-10-21 08:06:02 +0600

    Add a solution to the problem

--- sprint_3/testing/3_testing_according_to_plan.cpp
+++ sprint_3/testing/3_testing_according_to_plan.cpp
@@ -6,9 +6,34 @@ using namespace std;
 // Определяет, будет ли слово палиндромом
 // text может быть строкой, содержащей строчные буквы английского алфавита и пробелы
 // Пустые строки и строки, состоящие только из пробелов, - это не палиндромы
+string removeSpaces(const string& str) {
+    string result;
+    for (char c : str) {
+        if (!isspace(c)) {
+            result += c;
+        }
+    }
+    return result;
+}
+
+
 bool IsPalindrome(const string& text) {
     // Напишите недостающий код
-    return false;
+    string clear_text = removeSpaces(text);
+    if (clear_text.empty()) {
+        return false;
+    }
+    int left = 0;
+    int right = clear_text.size() - 1;
+    while (left < right)
+    {
+        if (clear_text[left] != clear_text[right]) {
+            return false;
+        }
+        ++left;
+        --right;
+    }
+    return true;
 }
 
 
