c06ff80c549cc8d1f333a62cf3bc04397989a358
SciWake <sciwake@gmail.com>
2023-11-19 10:15:03 +0600

    Add code template

---
+++ sprint_3/3_life_cycle_of_the_object/2_initialization_of_objects.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+using namespace std;
+
+class Rational {
+public:
+    int Numerator() const {
+        return numerator_;
+    }
+
+    int Denominator() const {
+        return denominator_;
+    }
+    
+    void SetNumerator(int numerator) {
+        numerator_ = numerator;
+    }
+
+    void SetDenominator(int denominator) {
+        if (denominator != 0) {
+            denominator_ = denominator;
+        }
+    }
+
+private:
+    int numerator_;
+    int denominator_;
+};
+
+int main() {
+    // Попробуйте угадать, что выведет эта программа
+    Rational r;
+    cout << r.Numerator() << "/"s << r.Denominator() << endl;
+}
