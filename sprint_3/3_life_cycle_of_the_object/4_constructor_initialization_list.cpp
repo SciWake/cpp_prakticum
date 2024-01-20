56248c14beb0e34181f7f9f9b207cf38577cc15e
SciWake <sciwake@gmail.com>
2024-01-21 00:48:28 +0600

    Problem solved

--- sprint_3/3_life_cycle_of_the_object/4_constructor_initialization_list.cpp
+++ sprint_3/3_life_cycle_of_the_object/4_constructor_initialization_list.cpp
@@ -1,19 +1,22 @@
 #include <numeric>
+#include <vector>
+#include <iostream>
 
 using namespace std;
+
 
 class Rational {
 public:
     Rational() = default;
 
-    Rational(int numerator) {
-        numerator_ = numerator;
-        denominator_ = 1;
+    Rational(int numerator)
+        : numerator_(numerator)
+        , denominator_(1) {
     }
 
-    Rational(int numerator, int denominator) {
-        numerator_ = numerator;
-        denominator_ = denominator;
+    Rational(int numerator, int denominator)
+        : numerator_(numerator)
+        , denominator_(denominator) {
         Normalize();
     }
 
@@ -39,3 +42,29 @@ private:
     int numerator_ = 0;
     int denominator_ = 1;
 };
+
+
+Rational AddRationals(Rational r1, Rational r2) {
+    int numerator = r1.Numerator() * r2.Denominator() + r2.Numerator() * r1.Denominator();
+    int denominator = r1.Denominator() * r2.Denominator();
+
+    // Компилятор знает, что функция возвращает Rational, и неявно
+    // вызывает соответствующий конструктор
+    return {numerator, denominator};
+    // Эта запись в данном контексте аналогична:
+    // return Rational{numerator, denominator};
+}
+
+int main() {
+    Rational zero;     // Дробь 0/1 = 0
+    const Rational seven(7); // Дробь 7/1 = 7
+    const Rational one_third(1, 3); // Дробь 1/3
+    vector<Rational> numbers;
+    numbers.push_back(Rational{7, 8});
+    // Следующие 2 строки эквивалентны - добавляют в numbers дробь 3/1
+    numbers.push_back(Rational{3});
+    numbers.push_back(3);
+    Rational sum = AddRationals(Rational{1,6}, one_third);
+    // Выведет 1/2
+    cout << sum.Numerator() << "/" << sum.Denominator();
+}
