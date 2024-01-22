3a5c1471ced28f3d37aa44fed3b8b5fc79634570
SciWake <sciwake@gmail.com>
2024-01-22 10:12:35 +0600

    Initialization of the Document class constructor

--- sprint_3/3_life_cycle_of_the_object/6_initializing_the_search_engine.cpp
+++ sprint_3/3_life_cycle_of_the_object/6_initializing_the_search_engine.cpp
@@ -46,9 +46,16 @@ vector<string> SplitIntoWords(const string& text) {
 }
 
 struct Document {
-    int id;
-    double relevance;
-    int rating;
+    Document() = default;
+    Document(int id, double relevance, int rating)
+        : id(id)
+        , relevance(relevance)
+        , rating(rating)
+    {
+    }
+    int id = 0;
+    double relevance = 0.0;
+    int rating = 0;
 };
 
 enum class DocumentStatus {
