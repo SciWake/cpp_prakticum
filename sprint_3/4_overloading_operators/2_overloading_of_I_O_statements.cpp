ce5571cb8380883a19c8d2978e2e12f393437628
SciWake <sciwake@gmail.com>
2024-01-25 18:25:22 +0600

    Add a main function to check

--- sprint_3/4_overloading_operators/2_overloading_of_I_O_statements.cpp
+++ sprint_3/4_overloading_operators/2_overloading_of_I_O_statements.cpp
@@ -65,3 +65,20 @@ Rational AddRationals(Rational r1, Rational r2) {
     // Эта запись в данном контексте аналогична:
     // return Rational{numerator, denominator};
 }
+
+int main() {
+    Rational zero;     // Дробь 0/1 = 0
+    const Rational seven(7); // Дробь 7/1 = 7
+    cout << zero << endl;
+    cin >> zero;
+    cout << zero << endl;
+    const Rational one_third(1, 3); // Дробь 1/3
+    vector<Rational> numbers;
+    numbers.push_back(Rational{7, 8});
+    // Следующие 2 строки эквивалентны - добавляют в numbers дробь 3/1
+    numbers.push_back(Rational{3});
+    numbers.push_back(3);
+    Rational sum = AddRationals(Rational{1,6}, one_third);
+    // Выведет 1/2
+    cout << sum.Numerator() << "/" << sum.Denominator();
+}
