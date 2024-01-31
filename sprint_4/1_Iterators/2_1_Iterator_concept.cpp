95166d6823fa645701948cdfbfb7e8333b3d6c43
SciWake <sciwake@gmail.com>
2024-01-31 16:04:57 -0500

    Solve a problem

--- sprint_4/1_Iterators/2_1_Iterator_concept.cpp
+++ sprint_4/1_Iterators/2_1_Iterator_concept.cpp
@@ -7,5 +7,9 @@ using namespace std;
 
 int main() {
     vector<string> langs = {"Python"s, "Java"s, "C#"s, "Ruby"s, "C++"s};
-    // Выведите первый язык, начинающийся на C, используя алгоритм find_if
+    auto result = find_if(langs.begin(), langs.end(), [](const string &lang) {
+        return lang[0] == 'C';
+    });
+    cout << *result << endl;
+    return 0;
 }
