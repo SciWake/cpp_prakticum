590e0e0c2a97032c8a806a12918e0966c21e1ae5
SciWake <sciwake@gmail.com>
2024-02-01 18:26:53 -0500

    Solve a problem

--- sprint_4/1_Iterators/2_2_Iterator_concept.cpp
+++ sprint_4/1_Iterators/2_2_Iterator_concept.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -12,4 +13,9 @@ struct Lang {
 int main() {
     vector<Lang> langs = {{"Python"s, 29}, {"Java"s, 24}, {"C#"s, 20}, {"Ruby"s, 25}, {"C++"s, 37}};
     // Выведите первый язык, начинающийся на J, используя алгоритм find_if
+    auto result = find_if(langs.begin(), langs.end(), [](const Lang &lang) {
+        return lang.name[0] == 'J';
+    });
+    cout << result->name << ", " << result->age << endl;
+    return 0;
 }
