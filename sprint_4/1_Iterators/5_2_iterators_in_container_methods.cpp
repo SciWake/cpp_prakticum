c6a0a089ef07c053a282e0f388b9d5d5f36ac7df
SciWake <sciwake@gmail.com>
2024-02-02 01:21:31 -0800

    Add initial code

---
+++ sprint_4/1_Iterators/5_2_iterators_in_container_methods.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+template <typename It>
+void PrintRange(It range_begin, It range_end) {
+    for (auto it = range_begin; it != range_end; ++it) {
+        cout << *it << " "s;
+    }
+    cout << endl;
+}
+
+template <typename Container, typename Iterator>
+void EraseAndPrint(Container& container, Iterator it) {
+    auto it_to_erased = container.erase(it);
+    PrintRange(container.begin(), it_to_erased);
+    PrintRange(it_to_erased, container.end());
+}
+
+int main() {
+    vector<string> langs = {"Python"s, "Java"s, "C#"s, "Ruby"s, "C++"s};
+    EraseAndPrint(langs, 2);
+    return 0;
+}
