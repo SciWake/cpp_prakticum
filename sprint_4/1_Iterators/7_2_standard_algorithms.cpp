97af7f5ae6b97425379256ba01a32e3d14fbcc93
SciWake <sciwake@gmail.com>
2024-02-04 03:12:57 +0600

    Update the program interaction interface

--- sprint_4/1_Iterators/7_2_standard_algorithms.cpp
+++ sprint_4/1_Iterators/7_2_standard_algorithms.cpp
@@ -31,13 +31,22 @@ vector<string> GetPermutations(It range_begin, It range_end) {
 
 
 int main() {
-    vector<int> permutation(3);
+    vector<int> test_vector(10);
     // iota             -> http://ru.cppreference.com/w/cpp/algorithm/iota
     // Заполняет диапазон последовательно возрастающими значениями
-    iota(permutation.begin(), permutation.end(), 1);
-    auto result = GetPermutations(permutation.begin(), permutation.end());
-    for (const auto& s : result) {
-        cout << s;
-    }
+    iota(test_vector.begin(), test_vector.end(), 1);
+
+    // shuffle   -> https://ru.cppreference.com/w/cpp/algorithm/random_shuffle
+    // Перемешивает элементы в случайном порядке
+    random_device rd;
+    mt19937 g(rd());
+    shuffle(test_vector.begin(), test_vector.end(), g);
+    
+    // Выводим вектор до сортировки
+    PrintRange(test_vector.begin(), test_vector.end());
+    // Сортируем вектор с помощью сортировки слиянием
+    MergeSort(test_vector.begin(), test_vector.end());
+    // Выводим результат
+    PrintRange(test_vector.begin(), test_vector.end());
     return 0;
 }
