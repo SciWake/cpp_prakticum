7a613c4a544e091660f21edfcc40343af57c2728
SciWake <sciwake@gmail.com>
2024-02-04 03:26:15 +0600

    Remove unwanted code

--- sprint_4/1_Iterators/7_2_standard_algorithms.cpp
+++ sprint_4/1_Iterators/7_2_standard_algorithms.cpp
@@ -1,33 +1,5 @@
-#include <algorithm>
-#include <iostream>
-#include <numeric>
-#include <sstream>
-#include <vector>
-
-using namespace std;
-
-// функция, записывающая элементы диапазона в строку
-template <typename It>
-string PrintRangeToString(It range_begin, It range_end) {
-    // удобный тип ostringstream -> https://ru.cppreference.com/w/cpp/io/basic_ostringstream
-    ostringstream out;
-    for (auto it = range_begin; it != range_end; ++it) {
-        out << *it << " "s;
-    }
-    out << endl;
-    // получаем доступ к строке с помощью метода str для ostringstream
-    return out.str();
-}
-
-template <typename It>
-vector<string> GetPermutations(It range_begin, It range_end) {
-    vector<string> permutations;
-    sort(range_begin, range_end);
-    do {
-        permutations.push_back(PrintRangeToString(range_begin, range_end));
-    } while (next_permutation(range_begin, range_end));
-    return permutations;
-}
+template <typename RandomIt>
+void MergeSort(RandomIt range_begin, RandomIt range_end)
 
 
 int main() {
