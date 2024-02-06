314809bd9e359dc0914c8566677f6c99abb92dc8
SciWake <sciwake@gmail.com>
2024-02-06 15:09:04 +0600

    Add initial code

---
+++ sprint_4/2_search_algorithms/2_search_in_unsorted_vector.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void PrintSpacesPositions(string& str) {
+    // напишите реализацию
+}
+
+int main() {
+    string str = "He said: one and one and one is three"s;
+    PrintSpacesPositions(str);
+    return 0;
+}
