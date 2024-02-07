20392e9b1976ca7ede564dfd0d5b5c4ffdaa2831
SciWake <sciwake@gmail.com>
2024-02-07 20:48:28 +0600

    Implement stack

--- sprint_4/3_stack_queue_dec/2_1_stack.cpp
+++ sprint_4/3_stack_queue_dec/2_1_stack.cpp
@@ -15,25 +15,25 @@ template <typename Type>
 class Stack {
 public:
     void Push(const Type& element) {
-        // напишите реализацию
+        elements_.push_back(element);
     }
     void Pop() {
-        // напишите реализацию
+        elements_.pop_back();
     }
     const Type& Peek() const {
-        // напишите реализацию
+        return elements_.back();
     }
     Type& Peek() {
-        // напишите реализацию
+        return elements_.back();
     }
     void Print() const {
-        // напишите реализацию
+        PrintRange(elements_.begin(), elements_.end());
     }
     uint64_t Size() const {
-        // напишите реализацию
+        return elements_.size();
     }
     bool IsEmpty() const {
-        // напишите реализацию
+        return elements_.empty();
     }
 
 private:
@@ -50,4 +50,5 @@ int main() {
         stack.Pop();
         stack.Print();
     }
+    return 0;
 }
