137c2acd233c95eddc38e6b56bf770a6e0c9d90b
SciWake <sciwake@gmail.com>
2024-02-10 04:56:02 +1200

    Add a task workpiece

---
+++ sprint_4/4_allocating_code_to_files/4_header_and_realization_files/wall.cpp
@@ -0,0 +1,8 @@
+Wall::Wall(double width, double height)
+    : width_(width)
+    , height_(height)
+    , color_(Color::WHITE) {
+}
+
+/*Напишите определение методов класса Wall здесь,
+пользуясь примером конструктора, данного выше*/
