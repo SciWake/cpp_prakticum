f58edd2eeb7fef972c60b5768dd9415820e35fc0
SciWake <sciwake@gmail.com>
2024-02-11 03:42:18 +1200

    Add the accountant.h file

--- sprint_4/4_allocating_code_to_files/6_problem_of_double_inclusion/main.cpp
+++ sprint_4/4_allocating_code_to_files/6_problem_of_double_inclusion/main.cpp
@@ -1,17 +1,22 @@
+#include "accountant.h"
 #include "builder.h"
-#include "painter.h"
 #include "carpenter.h"
+#include "painter.h"
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
     Builder tom;
-    Painter bill;
     Carpenter jack;
+    Accountant ray;
     Wall wall(3.5, 2.45);
 
-    cout << tom.CalcBricksNeeded(wall) << endl;
-    cout << bill.CalcPaintNeeded(wall) << endl;
-    cout << jack.CalcShelves(wall) << endl;
+    cout << ray.CalcBricksNeeded(wall) << endl;
+    cout << ray.CalcPaintNeeded(wall) << endl;
+
+    jack.InstallDoor(wall, tom);
+    cout << wall.IsDoorInstalled() << endl;
+    return 0;
 }
