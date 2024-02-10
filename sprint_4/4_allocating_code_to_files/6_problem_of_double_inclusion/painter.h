6eedd54868e8d122a4a633bfd96712741b39ad4b
SciWake <sciwake@gmail.com>
2024-02-11 03:46:21 +1200

    Implement the CalcPaintNeeded method

--- sprint_4/4_allocating_code_to_files/6_problem_of_double_inclusion/painter.h
+++ sprint_4/4_allocating_code_to_files/6_problem_of_double_inclusion/painter.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "square_calculation.h"
 #include "wall.h"
 
@@ -7,10 +6,5 @@ class Painter {
 public:
     void Paint(Wall& wall, Wall::Color color) const {
         wall.SetColor(color);
-    }
-    double CalcPaintNeeded(const Wall& wall) const {
-        double height = wall.GetHeight();
-        double width = wall.GetWidth();
-        return CalcSquare(width, height) * 0.4;
     }
 };
