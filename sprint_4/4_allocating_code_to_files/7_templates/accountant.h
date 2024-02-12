4a3707e54125aa6fa4baa01cfc40a434b3aeb0f1
SciWake <sciwake@gmail.com>
2024-02-12 18:13:04 +1200

    Fix an error at the file layout stage

--- sprint_4/4_allocating_code_to_files/7_templates/accountant.h
+++ sprint_4/4_allocating_code_to_files/7_templates/accountant.h
@@ -11,3 +11,15 @@ public:
     template <class BuildingObject>
     double CalcBricksNeeded(const BuildingObject& building_object) const;
 };
+
+template <class BuildingObject>
+double Accountant::CalcPaintNeeded(const BuildingObject& building_object) const {
+    std::pair<double, double> sizes = building_object.GetSizes();
+    return CalcSquare(sizes.first, sizes.second) * 0.4;
+}
+
+template <class BuildingObject>
+double Accountant::CalcBricksNeeded(const BuildingObject& building_object) const {
+    std::pair<double, double> sizes = building_object.GetSizes();
+    return CalcSquare(sizes.first, sizes.second) * 5;
+}
