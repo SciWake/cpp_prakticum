8e28d81d05ba10fd2eec94545630764fb61af44d
SciWake <sciwake@gmail.com>
2024-02-17 03:21:02 +0600

    using namespace literals

--- sprint_5/1_profiling_and_accelerating/_3_measuring_time.cpp
+++ sprint_5/1_profiling_and_accelerating/_3_measuring_time.cpp
@@ -4,17 +4,19 @@
 
 using namespace std;
 using namespace chrono;
+// хотите немного магии? тогда используйте namespace literals
+using namespace literals;
 
 int main() {
     cout << "Ожидание 5s..."s << endl;
     const auto start_time = steady_clock::now();
 
     // операция - ожидание 5 секунд
-    this_thread::sleep_for(seconds(5));
+    this_thread::sleep_for(5s);
     const auto end_time = steady_clock::now();
 
     const auto dur = end_time - start_time;
-    cerr << "Продолжительность сна: "s << duration_cast<milliseconds>(dur).count() << " ms"s << endl;
+    cerr << "Продолжительность сна: "s << chrono::duration_cast<chrono::milliseconds>(dur).count() << " ms"s << endl;
 
     cout << "Ожидание завершено"s << endl;
 }
