c5449a4d23e5a5ac87fe6672b751511b74cdfd25
SciWake <sciwake@gmail.com>
2024-02-19 16:48:07 +0600

    Apply past optimizations

--- sprint_5/1_profiling_and_accelerating/7_quantity_count/main.cpp
+++ sprint_5/1_profiling_and_accelerating/7_quantity_count/main.cpp
@@ -8,13 +8,7 @@
 using namespace std;
 
 vector<int> ReverseVector(const vector<int>& source_vector) {
-    vector<int> res;
-    
-    for (int i : source_vector) {
-        res.insert(res.begin(), i);
-    }
-
-    return res;
+    return {source_vector.rbegin(), source_vector.rend()};
 }
 
 int CountPops(const vector<int>& source_vector, int begin, int end) {
@@ -30,22 +24,27 @@ int CountPops(const vector<int>& source_vector, int begin, int end) {
 }
 
 void AppendRandom(vector<int>& v, int n) {
-    for (int i = 0; i < n; ++i) {
-        // получаем случайное число с помощью функции rand.
-        // в C++ имеются более современные генераторы случайных чисел,
-        // но в данном уроке не будем их касаться
-        v.push_back(rand() % 2);
+    for (int i = 0; i < n; i += 15) {
+        int number = rand();
+
+        // мы можем заполнить 15 элементов вектора,
+        // но не более, чем нам осталось до конца:
+        int count = min(15, n - i);
+
+        for (int j = 0; j < count; ++j)
+            // операцию сдвига битов вы уже видели в этой программе
+            // на этот раз двигаем вправо, чтобы нужный бит оказался самым последним
+            v.push_back((number >> j) % 2);
     }
 }
 
 void Operate() {
     LOG_DURATION("Total"s);
 
-    vector<int> random_bits;
-
     // операции << для целых чисел это сдвиг всех бит в двоичной
     // записи числа. Запишем с её помощью число 2 в степени 17 (131072)
     static const int N = 1 << 17;
+    vector<int> random_bits(N);
 
     // заполним вектор случайными числами 0 и 1
     
