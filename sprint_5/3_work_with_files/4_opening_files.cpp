2829c1c1d87a43b4c38203bd7a0387752b7cb112
SciWake <sciwake@gmail.com>
2024-02-20 22:59:42 +0600

    Calculate file size

--- sprint_5/3_work_with_files/4_opening_files.cpp
+++ sprint_5/3_work_with_files/4_opening_files.cpp
@@ -5,11 +5,22 @@
 
 using namespace std;
 
-// реализуйте эту функцию:
-size_t GetFileSize(string file);
+
+size_t GetFileSize(string file) {
+    ifstream inp(file);
+    if (!inp) {
+        return string::npos;
+    }
+
+    inp.seekg(0, ios::end);
+
+    size_t res = static_cast<size_t>(inp.tellg());
+
+    return res;
+}
 
 int main() {
-    ofstream("test.txt") << "123456789"s;
+    ofstream("test.txt"s) << "123456789"s;
     assert(GetFileSize("test.txt"s) == 9);
 
     ofstream test2("test2.txt"s);
