e54ca5449ab6efe1cc599423bb0d805b5e1e7f13
SciWake <sciwake@gmail.com>
2024-02-21 07:12:04 +0600

    Solve the problem

--- sprint_5/2_streaming_tricks/3_streaming_tricks.cpp
+++ sprint_5/2_streaming_tricks/3_streaming_tricks.cpp
@@ -6,12 +6,20 @@ using namespace std;
 
 class StreamUntier {
 public:
-    // добавьте конструктор, деструктор
-    // и дополнительные поля класса при необходимости
+    StreamUntier(istream& stream) 
+        : stream_(stream)
+        , tied_before_(stream.tie(nullptr))
+    {
+    }
 
+    ~StreamUntier() {
+        stream_.tie(tied_before_);
+    }
 private:
     ostream* tied_before_;
+    istream& stream_;
 };
+
 
 int main() {
     LOG_DURATION("\\n with tie"s);
