47defd625091bddba580abb9db5ecb16dda61bf0
SciWake <sciwake@gmail.com>
2024-02-22 03:29:25 +0600

    Add a binary flag

--- sprint_5/3_work_with_files/_5_binary_files_write.cpp
+++ sprint_5/3_work_with_files/_5_binary_files_write.cpp
@@ -12,13 +12,13 @@ int main(int argc, const char** argv) {
         return 1;
     }
 
-    ifstream in_file(argv[1]);
+    ifstream in_file(argv[1], ios::in | ios::binary);
     if (!in_file) {
         cerr << "Can't open input file"s << endl;
         return 2;
     }
 
-    ofstream out_file(argv[2]);
+    ofstream out_file(argv[2], ios::out | ios::binary);
     if (!out_file) {
         cerr << "Can't open output file"s << endl;
         return 2;
