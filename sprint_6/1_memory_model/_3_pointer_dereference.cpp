a677fb43612e49bdef22fb45219256d7c1487864
SciWake <sciwake@gmail.com>
2024-02-25 01:40:17 +0500

    Pointer dereference

---
+++ sprint_6/1_memory_model/_3_pointer_dereference.cpp
@@ -0,0 +1,29 @@
+#include <cassert>
+
+using namespace std;
+
+int main() {
+    int value = 1;
+
+    // Указатель value_ptr1, ссылающийся на переменную value
+    int* value_ptr1 = &value;
+    // Указатель value_ptr2, копия указателя value_ptr1, также ссылается на переменную value
+    int* value_ptr2 = value_ptr1;
+
+    // Значения указателей на один и тот же объект будут равны
+    assert(value_ptr1 == value_ptr2);
+
+    // Значение объекта value, полученное напрямую и через указатель на него, будет одно и то же
+    assert(*value_ptr1 == value && *value_ptr2 == value);
+
+    // Изменение value видно через указатели на него
+    value = 2;
+    assert(*value_ptr1 == value && *value_ptr2 == value);
+
+    // Изменяем значение value через указатель
+    *value_ptr2 = 3;
+
+    // Ожидаемо изменённое значение будет видно как при прямом доступе к объекту по его имени,
+    // так и при косвенном обращении через указатель value_ptr1
+    assert(*value_ptr1 == value && *value_ptr2 == value);
+}
