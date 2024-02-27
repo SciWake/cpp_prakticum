d6bd746a48c5605559fd49568db9abf7529eadcc
SciWake <sciwake@gmail.com>
2024-02-27 08:14:15 +0500

    Add an example of bad_alloc processing

---
+++ sprint_6/1_memory_model/_6_3_bad_alloc.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <new>
+#include <string>
+
+using namespace std;
+
+int main() {
+    size_t n = 0;
+    try {
+        // Так создаётся бесконечный цикл.
+        for (;;) {
+            // Внимание! Эта программа ведёт себя некорректно в иллюстративных целях.
+            // Указатель на выделенную в куче строку никуда не сохраняется, что
+            // приводит к утечке памяти.
+            new string(100'000'000, ' ');
+            ++n;
+        }
+    } catch (const bad_alloc&) {
+        // Рано или поздно доступная программе память закончится,
+        // и оператор new выбросит исключение bad_alloc
+        cout << "bad_alloc after "s << n << " allocations"s << endl;
+    }
+}
+
+// bad_alloc after 206 allocations
