afbdadf4c57b97e48822bc7eee550acfde14defa
SciWake <sciwake@gmail.com>
2024-03-04 19:29:19 +0500

    Use unordered_map

--- sprint_8/1_associative_containers/_3-1 Speeding up by avoiding sorting.cpp
+++ sprint_8/1_associative_containers/_3-1 Speeding up by avoiding sorting.cpp
@@ -51,7 +51,7 @@ int main() {
         cout << iter->first << " - "s << iter->second << endl;
     }
 }
-/*
+/* Output:
 Filling: 59 ms
 Moving & sorting: 3 ms
 Слово - Количество упоминаний в тексте
@@ -65,3 +65,66 @@ in - 3544
 his - 3103
 for - 3027
 a - 3025 */
+
+
+vector<pair<string, int>> GetSortedWordCounts(vector<string> words) {
+    unordered_map<string, int> counts_map;
+
+    {
+        LOG_DURATION("Filling"s);
+
+        for (auto& word : words) {
+            ++counts_map[move(word)];
+        }
+    }
+
+    cout << "Содержимое counts_map:"s << endl;
+    for (auto [iter, i] = tuple(counts_map.begin(), 0); i < 10 && iter != counts_map.end(); ++i, ++iter) {
+        cout << iter->first << " - "s << iter->second << endl;
+    }
+
+    {
+        LOG_DURATION("Moving & sorting"s);
+
+        vector<pair<string, int>> counts_vector(move_iterator(counts_map.begin()), move_iterator(counts_map.end()));
+        sort(counts_vector.begin(), counts_vector.end(), [](const auto& l, const auto& r) {
+            return l.second > r.second;
+        });
+
+        return counts_vector;
+    }
+}
+
+int main() {
+    vector<string> words;
+    string word;
+
+    while (cin >> word) {
+        words.push_back(word);
+    }
+
+    auto counts_vector = GetSortedWordCounts(move(words));
+
+    cout << "Слово - Количество упоминаний в тексте"s << endl;
+    // выводим первые 10 слов
+    for (auto [iter, i] = tuple(counts_vector.begin(), 0); i < 10 && iter != counts_vector.end(); ++i, ++iter) {
+        cout << iter->first << " - "s << iter->second << endl;
+    }
+}
+
+/* Output:
+Filling: 16 ms
+Содержимое counts_map:
+y-pilt - 1
+sitthen - 1
+ropes - 1
+sisours - 2
+assise - 2
+enquered - 1
+iustices - 2
+ferd - 1
+cheeke-boon - 1
+iustise - 3
+Moving & sorting: 3 ms
+...
+*/
