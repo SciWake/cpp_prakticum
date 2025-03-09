e34e06c0a0176fc17995ca0daae551e699494f6d
SciWake <SciWake@gmail.com>
2025-03-09 13:29:46 +0300

    study: add an example of using the while loop and logical operations

---
+++ sprint_0/3_conditions_and_cycles/_5_while_and_do_while.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    // Числа a и b должны быть натуральными
+    while (b != 0) {
+        int c = b;
+        b = a % b;
+        a = c;
+    }
+    cout << a << endl;
+}
