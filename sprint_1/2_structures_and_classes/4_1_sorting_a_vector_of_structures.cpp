bddb63ec897059e74cc1ce122ae4e3543da9736f
SciWake <SciWake@gmail.com>
2025-05-14 23:07:19 +0300

    Add a template

---
+++ sprint_1/2_structures_and_classes/4_1_sorting_a_vector_of_structures.cpp
@@ -0,0 +1,11 @@
+struct Document {
+    int id;
+    int relevance;
+};
+
+/* возвращаемое значение */  HasDocumentGreaterRelevance(/* параметры функции */) {
+    return /* условие, при котором первый объект будет в векторе раньше второго */;
+}
+
+int main() {
+}
