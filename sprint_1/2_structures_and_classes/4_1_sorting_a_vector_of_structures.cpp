140d3f85489fd014d6e837935b847df7457620b0
SciWake <SciWake@gmail.com>
2025-05-18 18:49:17 +0300

    The problem is solved

--- sprint_1/2_structures_and_classes/4_1_sorting_a_vector_of_structures.cpp
+++ sprint_1/2_structures_and_classes/4_1_sorting_a_vector_of_structures.cpp
@@ -1,11 +1,24 @@
+#include <vector>
+#include <algorithm>
+
 struct Document {
     int id;
     int relevance;
 };
 
-/* возвращаемое значение */  HasDocumentGreaterRelevance(/* параметры функции */) {
-    return /* условие, при котором первый объект будет в векторе раньше второго */;
+// Функция-компаратор для сортировки по убыванию релевантности
+bool HasDocumentGreaterRelevance(const Document& lhs, const Document& rhs) {
+    return lhs.relevance > rhs.relevance;
 }
 
 int main() {
+    std::vector<Document> documents = {
+        {100, 5},
+        {101, 8},
+        {102, 3}
+    };
+
+    std::sort(documents.begin(), documents.end(), HasDocumentGreaterRelevance);
+
+    // Теперь документы отсортированы по убыванию relevance: 8, 5, 3
 }
