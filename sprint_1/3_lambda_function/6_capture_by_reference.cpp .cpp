32de5fde5ae0d1eb217846de46ca72ca482904f1
SciWake <SciWake@gmail.com>
2025-06-11 17:49:40 +0300

    Solve the problem

---
+++ sprint_1/3_lambda_function/6_capture_by_reference.cpp .cpp
@@ -0,0 +1,33 @@
+#include <map>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int CountAndAddNewDogs(const vector<string>& new_dogs, const map<string, int>& max_amount,
+                       map<string, int>& shelter) {
+    return count_if(begin(new_dogs), end(new_dogs), [&shelter, &max_amount](const string& dog){
+        if (max_amount.at(dog) - shelter[dog] > 0) {
+            ++shelter[dog];
+            return true;
+        } 
+        return false;
+    });
+}
+
+int main() {
+    map<string, int> shelter {
+        {"shepard"s, 1},
+        {"corgie"s, 3},
+    };
+
+    map<string, int> max_amount {
+        {"shepard"s, 2},
+        {"corgie"s, 3},
+        {"shiba inu"s, 1},
+    };
+
+    cout << CountAndAddNewDogs({"shepard"s, "shiba inu"s, "shiba inu"s, "corgie"s}, max_amount, shelter) << endl;
+}
