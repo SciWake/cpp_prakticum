4ba0cf818c9e94ef4c1ab0730c996284354f2343
SciWake <SciWake@gmail.com>
2025-06-14 20:10:31 +0300

    Add a second version of the problem solution

--- sprint_1/3_lambda_function/5_capture_by_value.cpp.cpp
+++ sprint_1/3_lambda_function/5_capture_by_value.cpp.cpp
@@ -18,24 +18,38 @@ int ReadLineWithNumber() {
     return result;
 }
 
-vector<string> SplitIntoWords(const string& text) {
-    vector<string> words;
-    string word;
-    for (const char c : text) {
-        if (c == ' ') {
-            if (!word.empty()) {
-                words.push_back(word);
-                word.clear();
-            }
-        } else {
-            word += c;
-        }
-    }
-    if (!word.empty()) {
-        words.push_back(word);
-    }
+/* VERSION 1*/
+// vector<string> SplitIntoWords(const string& text) {
+//     vector<string> words;
+//     string word;
+//     for (const char c : text) {
+//         if (c == ' ') {
+//             if (!word.empty()) {
+//                 words.push_back(word);
+//                 word.clear();
+//             }
+//         } else {
+//             word += c;
+//         }
+//     }
+//     if (!word.empty()) {
+//         words.push_back(word);
+//     }
 
-    return words;
+//     return words;
+// }
+
+bool ContainsWord(const string& query, const string& buzzword) {
+    size_t pos = 0;
+    while ((pos = query.find(buzzword, pos)) != string::npos) {
+        bool left_ok = (pos == 0 || query[pos - 1] == ' ');
+        bool right_ok = (pos + buzzword.size() == query.size() || query[pos + buzzword.size()] == ' ');
+        if (left_ok && right_ok) {
+            return true;
+        }
+        pos += buzzword.size();
+    }
+    return false;
 }
 
 int main() {
@@ -47,12 +61,18 @@ int main() {
     }
     const string buzzword = ReadLine();
 
-    cout << count_if(queries.begin(), queries.end(), [buzzword](const string& query) {
-        for (const string& word : SplitIntoWords(query)) {
-            if (word == buzzword) return true;
-        }
-        return false;
+    /* VERSION 1*/
+    // cout << count_if(queries.begin(), queries.end(), [buzzword](const string& query) {
+    //     for (const string& word : SplitIntoWords(query)) {
+    //         if (word == buzzword) return true;
+    //     }
+    //     return false;
 
-    });
-    cout << endl;
+    // });
+
+    int result = count_if(queries.begin(), queries.end(), 
+        [&buzzword](const string& query) {
+            return ContainsWord(query, buzzword);
+        });
+    cout << result << endl;
 }
